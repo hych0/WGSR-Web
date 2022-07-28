13e103d59ab10dbeaa9fa3af8e97fbc3d2792b94
hych0 <74970802+hych0@users.noreply.github.com>
2022-07-28 10:35:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,32 +4,29 @@ import plotly.express as px
 import numpy as np
 import openpyxl
 
-
-
-
 ## --load dataframe
 
 # Contents of ~/my_app/streamlit_app.py
 import streamlit as st
 
 def page1():
-    st.markdown("Data Distribution")
+    st.markdown("Filter by Base and Temperature")
     #st.sidebar.markdown("# Data Distribution")
 
 def page2():
-    st.markdown("Filter by Base, T, Operating Conditions")
+    st.markdown("Filter by Operating Conditions")
     #st.sidebar.markdown("Filter️")
 
 
 page_names_to_funcs = {
-    "Data Distribution": page1,
+    "Filter by Base and Temperature": page1,
     "Filter by Base, T, Operating Conditions": page2,
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
 
-if selected_page == "Data Distribution":
+if selected_page == "Filter by Base and Temperature":
     excel_file = 'CategoricalWGSR_modified.xlsx'
     sheet_name = 'sheet1'
     df = pd.read_excel(excel_file,
@@ -40,11 +37,36 @@ if selected_page == "Data Distribution":
 #st.set_page_config(page_title='Data Distribution')
 
     st.subheader('Dataset')
-    df
+    #df
 
 # pie chart
 
+
+
+    #st.subheader('1.Filter by Base')
+    Base = df['Base'].unique().tolist()
+    base_selection = st.sidebar.multiselect('Base:',
+                                Base,
+                                default=Base)
+
+    df = (df[df["Base"].isin(base_selection)])
+
+
+
+    T = df['T'].unique().tolist()
+    min,max = st.sidebar.slider('T:',
+                           min_value= min(T),
+                           max_value=max(T),
+                           value=(min(T),max(T)))
+
+    df = df[(df['T'] >= min) & (df['T'] <= max)]
+    df
+    number_of_result = df.shape[0]
+    st.markdown(f'*Available Results: {number_of_result}*')
+
+
     st.subheader('Pie Charts ')
+
     df2 = df.groupby(['Base']).size().reset_index(name='Count')
     pie_chart = px.pie(df2,
                    title='Base',
@@ -109,7 +131,7 @@ if selected_page == "Filter by Base, T, Operating Conditions":
     df = pd.read_excel(excel_file,
                        sheet_name=sheet_name,
                        usecols='B:AA',
-                       header=1, engine='openpyxl')
+                       header=1,engine='openpyxl')
 
     # st.set_page_config(page_title='Data Distribution')
 
@@ -135,14 +157,15 @@ if selected_page == "Filter by Base, T, Operating Conditions":
                            value=(min(T),max(T)))
 
     df = df[(df['T'] >= min) & (df['T'] <= max)]
-    st.subheader("1. Filtered by Temperature and Base")
+    st.subheader("1. Dataset")
     df
     number_of_result = df.shape[0]
     st.markdown(f'*Available Results: {number_of_result}*')
 
 
-    st.subheader('2. Filter by Operating Conditions')
-    st.subheader('Operating Conditions of the Filtered Data')
+    #st.subheader('2. Filter by Operating Conditions')
+    st.subheader('2. Operating Conditions of the Filtered Data')
+    st.caption("The number of samples for each operating condition is available under the *count* column")
     operating_conditions = df.groupby(['H2','O2','CO','H2O','CO2','CH4','TOS','F/W']).size().reset_index().rename(columns={0:'count'})
     operating_conditions
     number_of_result = operating_conditions.shape[0]
